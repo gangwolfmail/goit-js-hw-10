1930633ebb9036bab4502595e14631cdd0ce0117
gangwolfmail <129858122+gangwolfmail@users.noreply.github.com>
2024-02-08 14:34:56 +0200

    Update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,34 +1,97 @@
 import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('.form');
 
-function createPromise(event) {
+form.addEventListener('submit', event => {
   event.preventDefault();
-  const delay = form.delay.value;
-  const state = form.state.value;
 
-  const promise = new Promise((resolve, reject) => {
-    if (state === 'fulfilled') {
-      setTimeout(() => resolve(delay), delay);
-    } else if (state === 'rejected') {
-      setTimeout(() => reject(delay), delay);
-    }
-  });
+  const time = form.elements.delay.value;
+  const state = form.elements.state.value;
 
-  promise
-    .then(delay => {
-      iziToast.success({
-        message: `✅ Fulfilled promise in ${delay}ms`,
-        position: 'bottomCenter',
-      });
-    })
-    .catch(delay => {
-      iziToast.error({
-        message: `❌Rejected promise in ${delay}ms`,
-        position: 'bottomCenter',
-      });
+  // if (time < 0) {
+  //   iziToast.show({
+  //     icon: 'icon-false',
+  //     backgroundColor: '#FC5A5A',
+  //     message: `Delay cannot be negative`,
+  //     messageColor: '#FAFAFB',
+  //     messageSize: '16px',
+  //     position: 'topCenter',
+  //     close: false,
+  //     closeOnClick: true,
+  //   });
+  //   return;
+  // }
+
+  function promise() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (state === 'fulfilled') {
+          resolve();
+        }
+        reject();
+      }, time);
     });
+  }
+
+  promise()
+    .then(value => successfulMessage(time))
+    .catch(error => errorMessage(time));
+});
+
+function errorMessage(delay) {
+  iziToast.show({
+    icon: 'icon-false',
+    backgroundColor: '#FC5A5A',
+    message: `Rejected promise in ${delay} ms`,
+    messageColor: '#FAFAFB',
+    messageSize: '16px',
+    position: 'topCenter',
+    close: false,
+  });
 }
 
-form.addEventListener('submit', createPromise);
+function successfulMessage(delay) {
+  iziToast.show({
+    icon: 'icon-false',
+    backgroundColor: '#82C43C',
+    message: `Fulfilled promise in ${delay} ms`,
+    messageColor: '#FAFAFB',
+    messageSize: '16px',
+    position: 'topCenter',
+    close: false,
+  });
+}
+// import iziToast from 'izitoast';
+// import 'izitoast/dist/css/iziToast.min.css';
+
+// const form = document.querySelector('.form');
+
+// function createPromise(event) {
+//   event.preventDefault();
+//   const delay = form.delay.value;
+//   const state = form.state.value;
+
+//   const promise = new Promise((resolve, reject) => {
+//     if (state === 'fulfilled') {
+//       setTimeout(() => resolve(delay), delay);
+//     } else if (state === 'rejected') {
+//       setTimeout(() => reject(delay), delay);
+//     }
+//   });
+
+//   promise
+//     .then(delay => {
+//       iziToast.success({
+//         message: `✅ Fulfilled promise in ${delay}ms`,
+//         position: 'bottomCenter',
+//       });
+//     })
+//     .catch(delay => {
+//       iziToast.error({
+//         message: `❌Rejected promise in ${delay}ms`,
+//         position: 'bottomCenter',
+//       });
+//     });
+// }
+
+// form.addEventListener('submit', createPromise);
